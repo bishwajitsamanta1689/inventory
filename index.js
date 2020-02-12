7647b9d0baca66966a55492baa5874246d071500
Bishwajeett Uday Samanta <bishwajit.samanta@smarsh.com>
2020-02-12 12:48:19 +0530

    Added the private ip

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello from the Server !!');
 });
 
-server.listen(8000,'127.0.0.1', () => {
+server.listen(8000,'172.31.34.107', () => {
   console.log('Server has been started to listen request on port 8000')
 })
