a9f1137d4bc6e3388d83dd9db5119594cc9505c5
Bishwajeett Uday Samanta <bishwajit.samanta@smarsh.com>
2020-02-12 12:29:31 +0530

    Added Hello from the Server

---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const http = require('http');
+
+// Server
+const server = http.createServer((req, res)) => {
+  res.end('Hello from the Server !!');
+};
+
+server.listen(8000,'127.0.0.1', () => {
+  console.log('Server has been started to listen request on port 8000')
+})
