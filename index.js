cdc370fc37d67a48edf7fcc0af6daf6f0c49763c
Bishwajeett Uday Samanta <bishwajit.samanta@smarsh.com>
2020-02-12 12:39:55 +0530

    Changes made

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const http = require('http');
 
 // Server
-const server = http.createServer((req, res)) => {
+const server = http.createServer((req, res) => {
   res.end('Hello from the Server !!');
-};
+});
 
 server.listen(8000,'127.0.0.1', () => {
   console.log('Server has been started to listen request on port 8000')
