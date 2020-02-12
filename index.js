4b018559cf2bf16cc914e464b668d5c6d52da80f
Bishwajeett Uday Samanta <bishwajit.samanta@smarsh.com>
2020-02-12 19:23:14 +0530

    Added Overview Page Functionality

--- index.js
+++ index.js
@@ -15,25 +15,43 @@ const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`)
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`)
 const dataObj = JSON.parse(data);
 
+//Replace Function Template
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+  output = output.replace(/{%IMAGE%}/g,product.image);
+  output = output.replace(/{%PRICE%}/g,product.price);
+  output = output.replace(/{%FROM%}/g,product.from);
+  output = output.replace(/{%NUTRIENTS%}/g,product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g,product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g,product.description);
+  output = output.replace(/{%ID%}/g,product.id);
+
+// Condition for Organic and Non Organic Items
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
+  return output;
+}
+
 //Create Server Call Back Function
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
-  //Overview Page
+//Overview Page
   if (pathName === '/' || pathName === '/overview') {
     res.writeHead(200, {'Content-type': 'text/html'});
-    res.end(tempOverview)
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard,el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+    res.end(output)
 
-    // Product Page
+// Product Page
   } else if (pathName === '/product') {
     res.end('This is from Product Page');
 
-    //API Route
+//API Route
   } else if (pathName === '/api') {
       res.writeHead(200, {'Content-type': 'application/json'});
       res.end (data);
 
-    //Not Found  
+//Not Found  
   }else {
     res.writeHead(404, {
       'Content-type': 'text/html',
@@ -43,7 +61,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-//Listening on Port 8000
+//Node CLI Output => Listening on Port 8000
 server.listen(8000,'172.31.34.107', () => {
   console.log('Server has been started to listen request on port 8000')
 })
