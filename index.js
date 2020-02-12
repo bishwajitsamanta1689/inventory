a699ac484f6abe67ed0c1d2016b73ee3322dde0b
Bishwajeett Uday Samanta <bishwajit.samanta@smarsh.com>
2020-02-12 13:29:04 +0530

    Added Json to the Browser

--- index.js
+++ index.js
@@ -12,9 +12,12 @@ const server = http.createServer((req, res) => {
   } else if (pathName === '/api') {
     fs.readFile(`${__dirname}/dev-data/data.json`,'utf-8', (err,data) => {
       const productData = JSON.parse(data);
-      console.log(productData);
+      res.writeHead(200, {
+        'Content-type': 'application/json'
+      })
+      res.end (data);
     })
-    res.end ('This is from API!!')
+    
   }else {
     res.writeHead(404, {
       'Content-type': 'text/html',
