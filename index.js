2c655e9b976b45f6bd535fd99c7a33e6ae4e7efc
Bishwajeett Uday Samanta <bishwajit.samanta@smarsh.com>
2020-02-12 16:59:41 +0530

    Added Annotation

--- index.js
+++ index.js
@@ -2,21 +2,35 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-// Server
+// Server Section
 
 //Data Declaration at the top level
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`);
+const tempOverview = fs.readFileSync(`${__dirname}/dev-data/data.json`);
+const tempCard = fs.readFileSync(`${__dirname}/dev-data/data.json`)
+const tempProduct = fs.readFileSync(`${__dirname}/dev-data/data.json`)
 const dataObj = JSON.parse(data);
 
+//Create Server
 const server = http.createServer((req, res) => {
   const pathName = req.url;
+
+  //Overview Page
   if (pathName === '/' || pathName === '/overview') {
+    
+
     res.end('This is from Overview Page!!')
+
+    // Product Page
   } else if (pathName === '/product') {
     res.end('This is from Product Page');
+
+    //API Route
   } else if (pathName === '/api') {
       res.writeHead(200, {'Content-type': 'application/json'});
       res.end (data);
+
+    //Not Found  
   }else {
     res.writeHead(404, {
       'Content-type': 'text/html',
